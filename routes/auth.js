4eb69e2dbae237e5d0c8f560fa23df82d64330c6
Dexter <mystyle1057@gmail.com>
2023-08-09 17:19:56 +0900

    230709 commit

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,12 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
 
+const low = require('lowdb');
+const FileSync  = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+// db.defaults({users:[]}).write();
+
 module.exports = function (passport) {
   router.get('/login', function (request, response) {
     var fmsg = request.flash();    
@@ -47,10 +53,10 @@ module.exports = function (passport) {
     var html = template.HTML(title, list, `
       <div style="color:red;">${feedback}</div>
       <form action="/auth/register_process" method="post">
-        <p><input type="text" name="email" placeholder="email"></p>
-        <p><input type="password" name="pwd" placeholder="password"></p>
-        <p><input type="password" name="pwd2" placeholder="password"></p>
-        <p><input type="text" name="displayName" placeholder="display name"></p>
+        <p><input type="text" name="email" placeholder="email" value="dexter@gmail.com"></p>
+        <p><input type="password" name="pwd" placeholder="password" value="1111"></p>
+        <p><input type="password" name="pwd2" placeholder="password" value="1111"></p>
+        <p><input type="text" name="displayName" placeholder="display name" value="Dexter"></p>
         <p>
           <input type="submit" value="register">
         </p>
@@ -58,6 +64,21 @@ module.exports = function (passport) {
     `, '');
     response.send(html);
   });
+
+  // router.post('/register_process', function(request, response) {
+  //   var post = request.body;
+  //   var email = post.email;
+  //   var pwd = post.pwd;
+  //   var pwd2 = post.pwd2;
+  //   var displayName = post.displayName;
+
+  //   db.get('users').push({
+  //     email:email,
+  //     password:pwd,
+  //     displayName:displayName
+  //   }).write();
+  //   response.redirect('/');
+  // });
   
   router.get('/logout', function (request, response) {
     request.logOut();
